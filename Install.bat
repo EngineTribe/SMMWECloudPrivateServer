5377e31573871dac909f62d8221c95a9ac7fcc32
YidaozhanYa <yidaozhanyaqwq@gmail.com>
2021-10-24 15:01:02 +0800

    1.0.3

--- Install.bat
+++ Install.bat
@@ -1,10 +1,48 @@
 @echo off
 color f0
 title SMMWE Cloud Private Server Installation
+set server-ver=1.0.3
 echo.
-echo. SMMWE Cloud Private Server - version 1.0.2
+echo. SMMWE Cloud Private Server - Version %server-ver%
 echo. By YidaozhanYa
 echo. -----------------------------------------------
+:chkinstall
+if exist "%USERPROFILE%\AppData\Local\SMM_WE_PrivateServer\Version.txt" (
+for /f "delims=" %%i in (%USERPROFILE%\AppData\Local\SMM_WE_PrivateServer\Version.txt) do (set server-ver-installed=%%i)&(goto :next)
+:next
+echo. Server version: %server-ver%
+echo. Installed version: %server-ver-installed%
+if "%server-ver-installed%"=="%server-ver%" (
+echo. You have already installed the Private Server.
+pause>nul
+exit
+)
+)
+:chkver
+ver | find "10.0" >nul
+if %errorlevel%==0 (
+    goto chkcaddy
+) else (
+ver | find "11.0" >nul
+if %errorlevel%==0 (
+    goto chkcaddy
+) else (
+    echo. This program only supports Windows 10/11.
+    echo, Please upgrade your system first.
+    pause>nul
+    exit
+)
+)
+:chkcaddy
+tasklist|find /i "caddy.exe">nul
+if %errorlevel%==0 (
+    echo. Please exit the server first.
+    pause>nul
+    exit
+) else (
+    goto chkadmin
+)
+:chkadmin
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' EQU '0' (
 goto INSTALLATION
@@ -25,6 +63,11 @@ echo. Installing SSL cert...
 echo. PLEASE CLICK "YES"
 "%~dp0certmgr.exe" -add -c "%~dp0smmwe_cloud.crt" -r localMachine -s trustedpublisher
 "%~dp0certmgr.exe" -add -c "%~dp0smmwe_cloud.crt" -s root
+echo. -----------------------------------------------
+echo. Saving installation information...
+mkdir "%USERPROFILE%\AppData\Local\SMM_WE_PrivateServer"
+mkdir "%USERPROFILE%\AppData\Local\SMM_WE_PrivateServer\Cache"
+echo %server-ver%>"%USERPROFILE%\AppData\Local\SMM_WE_PrivateServer\Version.txt"
 echo. -----------------------------------------------
 echo. Installation Completed!
 pause>nul
--- Uninstall.bat
+++ Uninstall.bat
@@ -1,10 +1,26 @@
 @echo off
 color f0
 title SMMWE Cloud Private Server Uninstallation
+set server-ver=1.0.3
 echo.
-echo. SMMWE Cloud Private Server - version 1.0.2
+echo. SMMWE Cloud Private Server - Version %server-ver%
 echo. By YidaozhanYa
 echo. -----------------------------------------------
+if exist "%USERPROFILE%\AppData\Local\SMM_WE_PrivateServer\Version.txt" (
+goto :chkcaddy
+) else (
+echo. You haven't installed the program yet.
+)
+:chkcaddy
+tasklist|find /i "caddy.exe">nul
+if %errorlevel%==0 (
+    echo. Please exit the server first.
+    pause>nul
+    exit
+) else (
+    goto chkadmin
+)
+:chkadmin
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' EQU '0' (
 goto UNINSTALLATION
@@ -22,6 +38,9 @@ echo. -----------------------------------------------
 echo. Removing SSL cert...
 "%~dp0certmgr.exe" -del -c "%~dp0smmwe_cloud.crt" -r localMachine -s trustedpublisher
 "%~dp0certmgr.exe" -del -c "%~dp0smmwe_cloud.crt" -s root
+echo. -----------------------------------------------
+echo. Removing configuration folder...
+rd /s /q "%USERPROFILE%\AppData\Local\SMM_WE_PrivateServer"
 echo. -----------------------------------------------
 echo. Unnstallation Completed!
 pause>nul
